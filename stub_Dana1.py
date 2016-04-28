65d08e6eeff71ad6c8388c50c11f7c40d5030a4d
DANA <dtomc@g.harvard.edu>
2016-04-28 13:04:51 -0400

    bug fixes

--- stub_Dana1.py
+++ stub_Dana1.py
@@ -1,6 +1,7 @@
 # Imports.
 import numpy as np
 import numpy.random as npr
+import pdb
 
 from SwingyMonkey import SwingyMonkey
 
@@ -17,7 +18,7 @@ class Learner(object):
         
         # Initialize discretizing size
         self.bin_width = 50
-        self.bin_height = 10
+        self.bin_height = 20
         self.alpha = 1
         self.gamma = 1
         self.gravity = None
@@ -25,15 +26,18 @@ class Learner(object):
         
         # Initialize Q-matrix to 0s
         num_actions = 2
-        self.tree_dist_array = [-np.inf ,-40, -30, -20, -10, 0]
+        self.tree_dist_array = [-np.inf ,-30, -10, 0]
         num_tree_dist = (600 / self.bin_width) + (len(self.tree_dist_array) - 1)
-        num_tree_height = 400 / self.bin_height
+        self.num_tree_height = (400 / self.bin_height) * 2
         num_monkey_height = 400 / self.bin_height
         self.vel_array = [-np.inf ,-40, -30, -20, -10, 0, 10, 20, 30, 40, np.inf]
         num_monkey_vel = len(self.vel_array)-1
         num_gravity = 3
         self.gravity_dict = {1:0, 4:1, None:2}
+        self.Q = np.zeros((num_actions,num_tree_dist,self.num_tree_height,num_monkey_vel, num_gravity))
+        '''
         self.Q = np.zeros((num_actions,num_tree_dist,num_tree_height,num_monkey_height,num_monkey_vel, num_gravity))
+        '''
     
     def velocity_bin(self, vel_array, monkey_vel):
         for i in range(len(vel_array)-1):
@@ -60,7 +64,7 @@ class Learner(object):
         Implement this function to learn things and take actions.
         Return 0 if you don't want to jump and 1 if you do.
         '''
-
+        #pdb.set_trace()
         # You might do some learning here based on the current state and the last state.
 
         # You'll need to select and action and return it.
@@ -68,23 +72,41 @@ class Learner(object):
         
         # Get Q-old from previous time
         if self.last_action != None:
+            Q_old = self.Q[self.last_action, self.state_D, self.state_T, self.state_V, self.state_G]
+            '''
             Q_old = self.Q[self.last_action, self.state_D, self.state_T, self.state_M, self.state_V, self.state_G]
+            '''
 
         # Get state space
         self.state_D = state['tree']['dist'] / self.bin_width # distance to tree
         if self.state_D < 0: # make distance non-negative
             self.state_D = self.tree_dist_bin(self.tree_dist_array, state['tree']['dist'])
+        self.state_T = (state['tree']['bot'] - state['monkey']['bot']) # height of bottom of tree
+        if self.state_T >= 0:
+            self.state_T = self.state_T / self.bin_height 
+        else:
+            self.state_T = (abs(self.state_T) / self.bin_height) + self.num_tree_height/2
+        #pdb.set_trace()
+        '''
         self.state_T = state['tree']['bot'] / self.bin_height # height of bottom of tree
         self.state_M = state['monkey']['bot'] / self.bin_height # height of bottom of monkey
+        '''
         self.state_V = self.velocity_bin(self.vel_array, state['monkey']['vel']) # monkey's velocity
         self.state_G = self.gravity_dict[self.gravity]
         
         if self.last_action != None:
+            Q_best = max(self.Q[:,self.state_D, self.state_T, self.state_V, self.state_G])
+            Q_new = Q_old + self.alpha * (self.last_reward + (self.gamma * Q_best) - Q_old)
+            print self.epoch, self.Q[self.last_action, self.state_D, self.state_T, self.state_V, self.state_G]
+            self.Q[self.last_action, self.state_D, self.state_T, self.state_V, self.state_G] = Q_new
+            print self.epoch, self.Q[self.last_action, self.state_D, self.state_T, self.state_V, self.state_G]
+            '''
             Q_best = max(self.Q[:,self.state_D, self.state_T, self.state_M, self.state_V, self.state_G])
             Q_new = Q_old + self.alpha * (self.last_reward + (self.gamma * Q_best) - Q_old)
             print self.epoch, self.Q[self.last_action, self.state_D, self.state_T, self.state_M, self.state_V, self.state_G]
             self.Q[self.last_action, self.state_D, self.state_T, self.state_M, self.state_V, self.state_G] = Q_new
             print self.epoch, self.Q[self.last_action, self.state_D, self.state_T, self.state_M, self.state_V, self.state_G]
+            '''
         
         # Check if this is the very beginning of the game
         if self.last_state == None or self.epoch == 1:
@@ -94,15 +116,20 @@ class Learner(object):
         else:
             #print True
             # Calculate the Qs for jumping or not
+            Q_stay = self.Q[0, self.state_D, self.state_T, self.state_V, self.state_G]
+            Q_jump = self.Q[1, self.state_D, self.state_T, self.state_V, self.state_G]
+            '''
             Q_stay = self.Q[0, self.state_D, self.state_T, self.state_M, self.state_V, self.state_G]
             Q_jump = self.Q[1, self.state_D, self.state_T, self.state_M, self.state_V, self.state_G]
+            '''
             if Q_stay == Q_jump: # if equal, choose randomly
-                new_action = npr.rand() < 0.1
+                new_action = npr.rand() < 0.2
             else:
                 # e-greedy
                 e_greedy = np.random.rand() > .9
                 if e_greedy == True:
-                    new_action = npr.rand() < 0.1
+                    new_action = npr.rand() < 0.2
+                    print True
                 else:
                     new_action = np.argmax([Q_stay,Q_jump])
         
